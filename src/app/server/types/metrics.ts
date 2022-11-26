6e85d66f3a4eb09b562dd0a35e5761070dfeb0ca
Raul Plesa <raul.plesa@gmail.com>
2022-11-26 16:56:37 +0100

    Added metrics type

---
+++ src/app/server/types/metrics.ts
@@ -0,0 +1,12 @@
+export type UnitMetric = {
+  id: string
+  title: string
+  description: string
+  value: number
+  minValue: number
+  maxValue: number
+  step: number
+  createdAt: Date
+  updatedAt: Date
+  domainId: string
+}
